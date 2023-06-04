e9608009389398cb4a108b54d99880c8f02679e7
Tinos Ps <tinosps@gmail.com>
2023-06-04 15:19:55 +0100

    Added clickables to editorBuilder

--- src/clipio_editor/scripts/editorBuilder.js
+++ src/clipio_editor/scripts/editorBuilder.js
@@ -1,8 +1,17 @@
 import spaceToUnderscoreClickable from "./clickables/spaceToUnderscoreClickable.js";
 import removeTrailingSpacesClickable from "./clickables/removeTrailingSpacesClickable.js";
+import uppercaseClickable from "./clickables/uppercaseClickable.js";
+import lowercaseClickable from "./clickables/lowercaseClickable.js";
+import removeFormattingClickable from "./clickables/removeFormattingClickable.js";
 import { ClickableFactory } from "./ClickableFactory.js";
 
-const clickables = [spaceToUnderscoreClickable, removeTrailingSpacesClickable];
+const clickables = [
+  spaceToUnderscoreClickable,
+  removeTrailingSpacesClickable,
+  uppercaseClickable,
+  lowercaseClickable,
+  removeFormattingClickable,
+];
 
 document.addEventListener("DOMContentLoaded", () => {
   const clickableFactory = new ClickableFactory();
