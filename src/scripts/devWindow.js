2427cbb7e8d11746dbf6c7148cc87c9610cfd53e
Tinos Ps <tinosps@gmail.com>
2023-06-11 14:22:46 +0100

    devInfo and devWindow tests

--- src/scripts/devWindow.js
+++ src/scripts/devWindow.js
@@ -1,125 +1,89 @@
 const { ipcRenderer, clipboard } = require("electron");
 const Store = require("electron-store");
+import { DevInfo, DevInfoItem } from "./devInfo.js";
+
 const store = new Store();
 
-function truncate(str, n) {
+function boxed(str, n) {
   return `<span class='boxed'>${
     str.length > n ? `${str.substr(0, n - 1)}&hellip;` : str
   }</span>`;
 }
 
-function disableDevWindow() {
-  store.set("show", false);
-}
-
-function hideDevWindow() {
-  ipcRenderer.send("minimize");
-}
-
-function createListItem(value, parentSelector) {
-  const listItem = document.createElement("li");
-  listItem.innerHTML = value;
-  document.querySelector(parentSelector).appendChild(listItem);
-  return listItem;
-}
-
-function createListItemPair(
-  key,
-  value,
-  parentSelector,
-  blink = false,
-  id = null
-) {
-  const listItem = createListItem(`${key}: ${value}`, parentSelector);
-  listItem.id = id;
-  if (blink) {
-    listItem.className = "blink";
-  }
-  return listItem;
-}
-
-function updatePerformance(uptime, ramList) {
-  setInterval(() => {
-    uptime.innerHTML = `Uptime: ${(
-      Math.round(process.uptime() * 100) / 100
-    ).toFixed(2)}s`;
-
-    const ramUsed = process.memoryUsage();
-    ramList.forEach((listItem) => {
-      const key = listItem.innerHTML.split(":")[0];
-      listItem.innerHTML = `${key}: ${(
-        Math.round((ramUsed[key] / 1024 / 1024) * 100) / 100
-      ).toFixed(2)} MB`;
-    });
-  }, 1000);
-}
-
-function handleClipboardChanges(clipio) {
-  setInterval(() => {
-    const currentClipboard = localStorage.getItem("clipboard_CURRENT");
-    if (clipboard.readText() !== currentClipboard) {
-      clipio.className = "blink";
-      clipio.innerHTML = `Clipio clipboard: ${truncate(
-        clipboard.readText(),
-        60
-      )}`;
-      localStorage.setItem("clipboard_CURRENT", clipboard.readText());
-    } else {
-      clipio.className = "";
-    }
-  }, 1000);
+function createDevInfoItem(key, value, id, runFunc) {
+  const item = new DevInfoItem(key, value);
+  item.isLive = true;
+  item.id = id;
+  item.class = "blink";
+  item.run = runFunc;
+  return item;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  // VERSIONS
-  createListItemPair(
-    "Clipio",
-    ipcRenderer.sendSync("get_app_version"),
-    "#versions"
-  );
-  Object.entries(process.versions).forEach(([key, value]) =>
-    createListItemPair(key, value, "#versions")
+  // Versions
+  const versionsDevInfo = new DevInfo("Versions");
+
+  versionsDevInfo.content.push(
+    new DevInfoItem("Clipio", ipcRenderer.sendSync("get_app_version"))
   );
 
-  // DETAILS
-  createListItemPair("Platform", process.platform, "#details");
-  createListItemPair("Architecture", process.arch, "#details");
-  createListItemPair("PID", process.pid, "#details");
-  const clipio = createListItemPair(
+  for (const [key, value] of Object.entries(process.versions)) {
+    versionsDevInfo.content.push(new DevInfoItem(key, value));
+  }
+
+  document.getElementById("main").appendChild(versionsDevInfo.buildHTML());
+
+  // Details
+  const detailsDevInfo = new DevInfo("Details");
+
+  detailsDevInfo.content.push(new DevInfoItem("Platform", process.platform));
+  detailsDevInfo.content.push(new DevInfoItem("Architecture", process.arch));
+  detailsDevInfo.content.push(new DevInfoItem("PID", process.pid));
+
+  const clipioClipboard = createDevInfoItem(
     "Clipio clipboard",
-    truncate(clipboard.readText(), 60),
-    "#details",
-    false,
-    "clipio_details"
+    boxed(clipboard.readText(), 60),
+    "clipio_details",
+    () => boxed(clipboard.readText(), 60)
   );
-  localStorage.setItem("clipboard_CURRENT", clipboard.readText());
 
-  // PERFORMANCE
-  const uptime = createListItemPair(
+  detailsDevInfo.content.push(clipioClipboard);
+
+  document.getElementById("main").appendChild(detailsDevInfo.buildHTML());
+
+  // Performance
+  const performanceDevInfo = new DevInfo("Performance");
+
+  const uptime = createDevInfoItem(
     "Uptime",
     `${(Math.round(process.uptime() * 100) / 100).toFixed(2)}s`,
-    "#performance",
-    true
+    "uptime",
+    () => `${(Math.round(process.uptime() * 100) / 100).toFixed(2)}s`
   );
+
+  performanceDevInfo.content.push(uptime);
 
   const ramUsed = process.memoryUsage();
-  const ramList = Object.keys(ramUsed).map((key) =>
-    createListItemPair(
+  for (const key of Object.keys(ramUsed)) {
+    const valueInMb = `${(
+      Math.round((ramUsed[key] / 1024 / 1024) * 100) / 100
+    ).toFixed(2)} MB`;
+    const element = createDevInfoItem(
       key,
-      `${(Math.round((ramUsed[key] / 1024 / 1024) * 100) / 100).toFixed(2)} MB`,
-      "#performance",
-      true
-    )
-  );
+      valueInMb,
+      `ram_${key}`,
+      () => valueInMb
+    );
 
-  // LIVE UPDATES
-  updatePerformance(uptime, ramList);
-  handleClipboardChanges(clipio);
+    performanceDevInfo.content.push(element);
+  }
+
+  document.getElementById("main").appendChild(performanceDevInfo.buildHTML());
+
+  // Live updates
+  setInterval(() => {
+    versionsDevInfo.tick();
+    detailsDevInfo.tick();
+    performanceDevInfo.tick();
+  }, 1000);
 });
-
-// UNIT TESTING
-module.exports = {
-  truncate,
-  createListItem,
-  createListItemPair,
-};
---
+++ testing/DevInfo.test.js
@@ -0,0 +1,76 @@
+import { DevInfo, DevInfoItem } from "../src/scripts/DevInfo.js";
+
+// Mock document.createElement and document.getElementById
+beforeAll(() => {
+  document.createElement = jest.fn((tag) => {
+    return { tagName: tag, appendChild: jest.fn(), innerHTML: "" };
+  });
+
+  document.getElementById = jest.fn((id) => {
+    return { innerHTML: "" };
+  });
+});
+
+describe("DevInfo", () => {
+  it("should be able to create a new instance", () => {
+    const devInfo = new DevInfo("test");
+    expect(devInfo).toBeInstanceOf(DevInfo);
+    expect(devInfo.title).toBe("test");
+  });
+
+  it("should be able to add new DevInfoItem to content", () => {
+    const devInfo = new DevInfo("test");
+    const devInfoItem = new DevInfoItem("title", "value");
+    devInfo.content.push(devInfoItem);
+
+    expect(devInfo.content.length).toBe(1);
+    expect(devInfo.content[0]).toBeInstanceOf(DevInfoItem);
+  });
+
+  it("should be able to build HTML", () => {
+    const devInfo = new DevInfo("test");
+    const html = devInfo.buildHTML();
+
+    expect(html).toBeDefined();
+    expect(html.tagName).toBe("div");
+  });
+
+  it("should be able to call tick method", () => {
+    const devInfo = new DevInfo("test");
+    const devInfoItem = new DevInfoItem("title", "value");
+    devInfoItem.isLive = true;
+    devInfoItem.run = jest.fn(() => "new value");
+    devInfoItem.update = jest.fn();
+    devInfo.content.push(devInfoItem);
+
+    devInfo.tick();
+
+    expect(devInfoItem.run).toHaveBeenCalled();
+    expect(devInfoItem.update).toHaveBeenCalled();
+  });
+});
+
+describe("DevInfoItem", () => {
+  it("should be able to create a new instance", () => {
+    const devInfoItem = new DevInfoItem("title", "value");
+    expect(devInfoItem).toBeInstanceOf(DevInfoItem);
+    expect(devInfoItem.title).toBe("title");
+    expect(devInfoItem.value).toBe("value");
+  });
+
+  it("should be able to build HTML", () => {
+    const devInfoItem = new DevInfoItem("title", "value");
+    const html = devInfoItem.buildHTML();
+
+    expect(html).toBeDefined();
+    expect(html.tagName).toBe("li");
+  });
+
+  it("should be able to update HTML", () => {
+    const devInfoItem = new DevInfoItem("title", "value");
+    devInfoItem.id = "test";
+    devInfoItem.update();
+
+    expect(document.getElementById).toHaveBeenCalledWith("test");
+  });
+});
