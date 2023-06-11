cc0c3c028bfea22b2a17c1950bfae9c67bacd954
Tinos Ps <tinosps@gmail.com>
2023-06-11 15:01:37 +0100

    Reformatting + path bug fix

--- testing/devWindow.test.js
+++ testing/devWindow.test.js
@@ -1,47 +1,49 @@
 const { ipcRenderer, clipboard } = require("electron");
 const Store = require("electron-store");
-const { DevInfo, DevInfoItem } = require('../src/scripts/devInfo.js');
+const { DevInfo, DevInfoItem } = require("../src/scripts/DevInfo.js");
 
-jest.mock('electron', () => ({
+jest.mock("electron", () => ({
   ipcRenderer: { sendSync: jest.fn() },
   clipboard: { readText: jest.fn() },
 }));
 
-jest.mock('electron-store');
+jest.mock("electron-store");
 
 beforeAll(() => {
   global.process = {
-    versions: { electron: '12.0.0' },
-    platform: 'darwin',
-    arch: 'x64',
+    versions: { electron: "12.0.0" },
+    platform: "darwin",
+    arch: "x64",
     pid: 12345,
     uptime: jest.fn(),
     memoryUsage: jest.fn(),
   };
 
-  document.createElement = jest.fn().mockImplementation(tag => ({
+  document.createElement = jest.fn().mockImplementation((tag) => ({
     tagName: tag,
     appendChild: jest.fn(),
-    innerHTML: '',
+    innerHTML: "",
   }));
 
-  document.getElementById = jest.fn().mockReturnValue({ appendChild: jest.fn() });
+  document.getElementById = jest
+    .fn()
+    .mockReturnValue({ appendChild: jest.fn() });
   document.addEventListener = jest.fn((_, cb) => cb());
 });
 
-describe('DOMContentLoaded event', () => {
-  it('should create DevInfo and DevInfoItem instances and call their methods', () => {
-    ipcRenderer.sendSync.mockReturnValue('1.0.0');
-    clipboard.readText.mockReturnValue('clipboard text');
+describe("DOMContentLoaded event", () => {
+  it("should create DevInfo and DevInfoItem instances and call their methods", () => {
+    ipcRenderer.sendSync.mockReturnValue("1.0.0");
+    clipboard.readText.mockReturnValue("clipboard text");
     process.uptime.mockReturnValue(123456.789);
     process.memoryUsage.mockReturnValue({ rss: 123456789 });
 
-    require('../src/scripts/devWindow.js'); // replace with the path to your file
+    require("../src/scripts/devWindow.js"); // replace with the path to your file
 
-    expect(ipcRenderer.sendSync).toHaveBeenCalledWith('get_app_version');
+    expect(ipcRenderer.sendSync).toHaveBeenCalledWith("get_app_version");
     expect(clipboard.readText).toHaveBeenCalledTimes(1);
     expect(process.uptime).toHaveBeenCalled();
     expect(process.memoryUsage).toHaveBeenCalled();
-    expect(document.getElementById).toHaveBeenCalledWith('main');
+    expect(document.getElementById).toHaveBeenCalledWith("main");
   });
 });
