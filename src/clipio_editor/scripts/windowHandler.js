3fd5c0bba1992d05f422b924f30c2b2a2db09e82
Tinos Ps <tinosps@gmail.com>
2023-06-28 15:45:59 +0100

    Created settings popup

--- src/clipio_editor/scripts/windowHandler.js
+++ src/clipio_editor/scripts/windowHandler.js
@@ -1,6 +1,5 @@
 const { ipcRenderer } = require("electron");
 
-
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("maxi").addEventListener("click", function () {
     ipcRenderer.send("maximize");
@@ -14,5 +13,17 @@ document.addEventListener("DOMContentLoaded", () => {
         window.close();
       }
     }
+  });
+
+  // The same for element "settings"
+  document.getElementById("settings").addEventListener("click", function () {
+    const settings_width = 600;
+    const settings_height = 400;
+
+    window.open(
+      "../settings/settings.html",
+      "_blank",
+      `width=${settings_width},height=${settings_height},frame=true,autoHideMenuBar=true, nodeIntegration=yes,contextIsolation=false,`
+    );
   });
 });
