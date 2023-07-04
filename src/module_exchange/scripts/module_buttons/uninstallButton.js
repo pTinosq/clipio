5f8be01cd16c9d45e7f98dec815b5c6c5aed37d4
Tinos Ps <tinosps@gmail.com>
2023-07-04 02:05:15 +0100

    Added optional reload flag to uninstall function

--- src/module_exchange/scripts/module_buttons/uninstallButton.js
+++ src/module_exchange/scripts/module_buttons/uninstallButton.js
@@ -1,10 +1,11 @@
 const { ipcRenderer } = require("electron");
 
-export function uninstallModule(uid) {
+export function uninstallModule(uid, reload = true) {
   const localModules = ipcRenderer.sendSync("get-local-modules");
   delete localModules[uid];
   ipcRenderer.send("set-local-modules", localModules);
 
   ipcRenderer.send("delete-module", uid);
-  window.location.reload();
+
+  if (reload) window.location.reload();
 }
